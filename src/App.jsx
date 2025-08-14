d0b2b130f595079291d0b03c17c5ebeb3a14f021
EkramjemalH <ekramjemalh@gmail.com>
2025-08-14 15:54:01 +0300

    import category component to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
+import CategorySelect from "./components/CategorySelect";
 
 export default function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+         <Route path="/CategorySelect" element={<CategorySelect />} />
+
       </Routes>
     </Router>
   );
